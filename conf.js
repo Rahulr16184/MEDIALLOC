a8063a507e3c6e1ad410b53f52c716f41267de76
Rahul R <rahulr16184@gmail.com>
2025-09-14 23:29:45 +0530

    Update conf.js

--- conf.js
+++ conf.js
@@ -1,16 +1,10 @@
-// This file will hold your application's configuration.
-
-// Example for when you add Firebase:
-/*
+// Your web app's Firebase configuration, taken from your Firebase project settings.
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyDcLA-1YnLIl974BrWMnKkdFBd-zBpEshY",
+  authDomain: "medialloc.firebaseapp.com",
+  projectId: "medialloc",
+  storageBucket: "medialloc.firebasestorage.app",
+  messagingSenderId: "900158847557",
+  appId: "1:900158847557:web:16f56ee5b8656f5db3c12c",
+  measurementId: "G-C8Y5JRTXBR"
 };
-
-// Initialize Firebase
-// firebase.initializeApp(firebaseConfig);
-*/
