5d3d41492147f04f49129f5141803c13b68fab25
Rahul R <rahulr16184@gmail.com>
2025-09-14 22:18:47 +0530

    Create conf.js

---
+++ conf.js
@@ -0,0 +1,16 @@
+// This file will hold your application's configuration.
+
+// Example for when you add Firebase:
+/*
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+// Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
+*/
